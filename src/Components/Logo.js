6e559f52e9513fdbf9d96e452d78cb6d107c64dd
Sanjeev <sanjeev.ksv@gmail.com>
2024-12-09 16:06:46 +0530

    signup changes

---
+++ src/Components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+export default function Logo() {
+  return (
+    <div className="column1">
+      <div className="img">
+        <img src="bird.png" />
+        <h2>Hash Tech</h2>
+        <p>Powered by C# accedamy</p>
+      </div>
+    </div>
+  );
+}
