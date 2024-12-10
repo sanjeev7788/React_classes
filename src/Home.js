e0c9579153920044631ddbe956e18e558d3d534a
Sanjeev <sanjeev.ksv@gmail.com>
2024-12-10 15:15:35 +0530

    Homepage

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,24 @@
-import React from "react"
-function Home(){
+import React, { useEffect } from "react";
+import "./home.css";
 
-    return(
-     <div>
-        Home
-     </div>
-    )
+function Home() {
+  useEffect(() => {
+
+    async function fetchApidata() {
+      const response =await fetch("https://jsonplaceholder.typicode.com/todos");
+      const apidata = response.json();
+      console.log(apidata);
+    }
+    fetchApidata()
+
+  },[]);
+
+  return (
+    <div>
+      <center>
+        <h2>Home</h2>
+      </center>
+    </div>
+  );
 }
-export default Home
+export default Home;
